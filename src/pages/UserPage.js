519cfe6d7911679d1599373ee9f6972ebc6aeb0f
AlfiMuharom <Alfimuharom65@gmail.com>
2023-07-24 09:56:26 +0700

    uptade

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -69,7 +69,7 @@ const UserPage = () => {
                             {products.map((product) => (
                                 <li key={product._id}>
                                     <div>
-                                        <img src={'http://localhost:3001/img/' + product.image} alt="Product Image" />
+                                        <img src={'https://backend-iota-green.vercel.app/img/' + product.image} alt="Product Image" />
                                     </div>
                                     <div>
                                         <strong>{product.name}</strong> - {product.price}
